111292657076769418b72b59f546ef5978642f2b
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-14 15:31:51 +0000

    feat(devenv): Add initial support for `devenv`
    Soon to come - Postgres.

---
+++ devenv.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+}
